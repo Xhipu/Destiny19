008e440e586f7b7ae2f6f8a9322c140b350f6e24
itlives <42418283+itlives@users.noreply.github.com>
2019-02-07 11:10:53 +0100

    button hover wenn geklicked fixed

--- ui/UIButton.java
+++ ui/UIButton.java
@@ -5,12 +5,37 @@ import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class UIButton extends UIElement {
 	private static final long serialVersionUID = 1603212344675060032L;
 
 	public UIButton(String label, int x, int y, int w, int h) {
 		super(x,y,w,h);
+		
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				setBackground(Color.MAGENTA);
+			}
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				setBackground(Color.RED);
+			}
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				setBackground(Color.GRAY);
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if ( (e.getX() >= x && e.getX() <= x+w) && (e.getY() >= y && e.getY() <= y+h) ) {
+					setBackground(Color.MAGENTA);
+				} else {
+					setBackground(Color.red);
+				}
+			}
+		});
 		
 		JLabel lblText = new JLabel(label);
 		lblText.setFont(new Font("Microsoft YaHei UI Light", Font.BOLD, 35));
--- ui/UIElement.java
+++ ui/UIElement.java
@@ -2,8 +2,6 @@ package de.destiny19.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
@@ -13,24 +11,6 @@ public class UIElement extends JPanel {
 
 	
 	public UIElement(int x, int y, int w, int h) {
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				setBackground(Color.MAGENTA);
-			}
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				setBackground(Color.RED);
-			}
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				setBackground(Color.GRAY);
-			}
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				setBackground(Color.MAGENTA);
-			}
-		});
 		setBorder(new BevelBorder(BevelBorder.RAISED, new Color(255, 255, 0), new Color(255, 250, 205), new Color(255, 215, 0), new Color(255, 165, 0)));
 		setForeground(Color.WHITE);
 		setBackground(Color.RED);
